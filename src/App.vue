7a7e7ccfe8af522783e69f12343e470145d0606b
zizik <zizidukabrik@gmail.com>
2017-10-13 09:23:32 +0300

    Add header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <main-header></main-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+
 export default {
   name: "app",
+  components: {
+    mainHeader: Header,
+  },
 };
 </script>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    {{header}}
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      header: "My header",
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
