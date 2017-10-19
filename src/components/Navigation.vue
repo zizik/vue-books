25a085d2ec20d2566cab1731c474fa2426c3f953
zizik <zizidukabrik@gmail.com>
2017-10-20 00:57:11 +0300

    Add nav styles

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,23 +1,41 @@
 <template>
   <nav class="navigation">
-    <ul>
-      <li class="navigation__item">1</li>
-      <li class="navigation__item">2</li>
-      <li class="navigation__item">3</li>
+    <ul class="navigation__list">
+      <li class="navigation__list-item">
+        <router-link class="navigation__link" :to="{name: 'Books'}" href="/books">Books</router-link>
+      </li>
+      <li class="navigation__list-item">
+        <router-link class="navigation__link" :to="{name: 'Form'}" href="/form">Add New Book</router-link>
+      </li>
     </ul>
   </nav>
 </template>
 
 <script>
-export default {
-
-};
+export default {};
 </script>
 
 <style lang="scss">
 .navigation {
   width: 200px;
-  background-color: #ff0055;
+  height: 100%;
+  background-color: #f6f6f6;
   margin-left: $gutter;
+  box-shadow: $box-shadow;
+  border: $border;
+
+  &__link {
+    display: block;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 6px 12px;
+    border-bottom: $border;
+    text-decoration: none;
+    color: #739931;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 }
 </style>
