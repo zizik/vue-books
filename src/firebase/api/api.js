8a011dcb916eaed82dddb0bc766a31b41632c6c7
zizik <zizidukabrik@gmail.com>
2017-10-27 19:13:30 +0300

    Changed api patj

--- src/firebase/api/api.js
+++ src/firebase/api/api.js
@@ -1,14 +1,4 @@
-import * as firebase from "firebase";
-
-const config = {
-  apiKey: "AIzaSyAFwiSAedClmIi5jbkRMf-n765gUz2ye1c",
-  authDomain: "v-books-d1b6d.firebaseapp.com",
-  databaseURL: "https://v-books-d1b6d.firebaseio.com",
-  projectId: "v-books-d1b6d",
-  storageBucket: "v-books-d1b6d.appspot.com",
-  messagingSenderId: "866011591496",
-};
-firebase.initializeApp(config);
+import firebase from "../firebase";
 
 class FirebaseApi {
   constructor() {
