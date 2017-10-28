a37ab8a38bc7b96d222afec0862790cad596a155
zizik <zizidukabrik@gmail.com>
2017-10-28 09:34:46 +0300

    Added auth module

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,5 +51,6 @@ module.exports = {
         ignorePropertyModificationsFor: ["state"],
       },
     ],
+    "arrow-parens": "off",
   },
 };
---
+++ src/firebase/auth/auth.js
@@ -0,0 +1,73 @@
+import firebase from "../firebase";
+
+class User {
+  constructor() {
+    this.auth = firebase.auth();
+    this.isLogged = false;
+    this.checkUser();
+    console.log(this.isLogged);
+  }
+
+  async checkUser() {
+    await new Promise(resolve => {
+      this.auth.onAuthStateChanged(user => {
+        if (user) {
+          this.isLogged = true;
+          console.log("has user");
+        } else {
+          this.isLogged = false;
+          console.log("no user");
+        }
+        resolve();
+      });
+    });
+    return this.isLogged;
+  }
+
+  createUser(email, password) {
+    this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(good => console.log("good", good))
+      .catch(error => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+  }
+
+  authUser(email, password) {
+    return (
+      this.auth
+        .signInWithEmailAndPassword(email, password)
+        // .then(good => console.log("good", good))
+        .catch(error => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode);
+          console.log(errorMessage);
+          // ...
+        })
+    );
+  }
+
+  signOut() {
+    this.auth.signOut().then(
+      () => {
+        console.log("Signed Out");
+      },
+      error => {
+        console.error("Sign Out Error", error);
+      },
+    );
+  }
+
+  getUser() {
+    return this.auth.currentUser.uid;
+  }
+}
+
+export default new User();
