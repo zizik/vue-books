dfd6dd3c15cbb8a9cec8758814ee77e690a6de76
zizik <zizidukabrik@gmail.com>
2017-10-28 09:28:46 +0300

    Added sigIn component

---
+++ src/components/SignIn.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <input type="text" v-model="email" placeholder="email">
+    <input type="text" v-model="password" placeholder="password">
+    <button @click.prevent="logIn">Залогироваться</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "zizidukabrik@gmail.com",
+      password: "123456",
+    };
+  },
+  methods: {
+    logIn() {
+      console.log("logIn");
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
