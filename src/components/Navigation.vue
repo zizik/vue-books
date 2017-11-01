45f36a71fc3ed0d8cb9716ee03a5269d1951067c
zizik <zizidukabrik@gmail.com>
2017-11-01 15:18:39 +0300

    Renamed navigation create account link

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -8,7 +8,7 @@
         <router-link class="navigation__link" :to="{name: 'Form'}">Add New Book</router-link>
       </li>
       <li class="navigation__list-item">
-        <router-link class="navigation__link" :to="{name: 'SignIn'}">Залогироваться</router-link>
+        <router-link class="navigation__link" :to="{name: 'SignIn'}">Создать аккаунт</router-link>
       </li>
     </ul>
   </nav>
